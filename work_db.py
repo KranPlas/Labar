f90c246c45315ffe5f08ca4267edfc21b880f4e8
KranPlas <karapuchka2007@gmail.com>
2025-11-20 23:57:49 +0300

    Обновлен файл work_db.py

--- work_db.py
+++ work_db.py
@@ -1,24 +1,59 @@
 import sqlite3
+from datetime import datetime
+
 
 # Создание базы данных (файл)
 conn = sqlite3.connect('labar.db')
 cursor = conn.cursor()
 
-# Создание таблицы Компьютер
+# Создание таблицы
 cursor.execute('''
-CREATE TABLE IF NOT EXISTS Users(
+CREATE TABLE IF NOT EXISTS Users (
     user_id INTEGER PRIMARY KEY,
-    username TEXT,
+    name TEXT NOT NULL,
     email TEXT NOT NULL,
     role TEXT NOT NULL,
-    created_at TIMESTAMP NOT NULL
+    created_at TEXT NOT NULL
 )
 ''')
 
 # Добавление записи
-cursor.execute("INSERT INTO Users (username) VALUES ('KranPlas')")
-cursor.execute("INSERT INTO Users (email) VALUES ('karapuchka2007@gmail.com')")
-cursor.execute("INSERT INTO Users (role) VALUES ('admin')")
+cursor.execute('''
+INSERT INTO Users (name, email, role, created_at) 
+VALUES (?, ?, ?, ?)
+''', ('homyacheLLo', 'hgkfgn75fv@gmail.com', 'user', datetime.now()))
+
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS PCs (
+    pc_id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    inst_date TEXT NOT NULL,
+    inst_place TEXT NOT NULL
+)
+''')
+
+cursor.execute('''
+INSERT INTO PCs (name, inst_date, inst_place) 
+VALUES (?, ?, ?)
+''', ('PC-666KIR', '19.09.2013', 'C:'))
+
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Programs (
+    software_id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    version TEXT NOT NULL,
+    release_date TEXT NOT NULL,
+    developer TEXT NOT NULL,
+    category TEXT NOT NULL
+)
+''')
+
+cursor.execute('''
+INSERT INTO Programs (name, version, release_date, developer, category) 
+VALUES (?, ?, ?, ?, ?)
+''', ('Steam', 'Unknown', '12.09.2003', 'Valve', 'Games'))
 
 # Получение данных
 cursor.execute("SELECT * FROM Users")
